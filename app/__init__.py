10fff9763a9f76e53e37173222419ee4982744ea
Ivan Galakhov <igalakhov00@stuy.edu>
2019-04-07 17:36:50 -0400

    began database work

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from config import *
 from app.routes import app
+from app.models import db
 
 app.config['SECRET_KEY'] = 'secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+with app.test_request_context():
+    db.init_app(app)
+    db.create_all()
+
+print(db)
+print(db.metadata.tables.keys())
