5cfe035ce46d084e6fcd2f2dca5f6de0c99f91dc
Ivan Galakhov <igalakhov00@stuy.edu>
2019-04-07 23:22:11 -0400

    more model work

--- app/models/models.py
+++ app/models/models.py
@@ -1,12 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
+from config import *
+from app import app
+from passlib.hash import bcrypt
 
-db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 
+# models
 class User(db.Model):
+
+    # single fields
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    first = db.Column(db.String(127), nullable=False)
+    last = db.Column(db.String(127), nullable=False)
+    username = db.Column(db.String(127), unique=True, nullable=False)
+    email = db.Column(db.String(127), unique=True, nullable=False)
+    password = db.Column(db.String(127), nullable=False)
+
+    # relationships
+
+    def __init__(self, first, last, username, email, password):
+        self.first = first
+        self.last = last
+        self.username = username
+        self.email = email
+        self.password = bcrypt.encrypt(password)
+
+    def validate_password(self, password):
+        return bcrypt.verify(password, self.password)
 
     def __repr__(self):
         return '<User %r>' % self.username
