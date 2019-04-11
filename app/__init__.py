cc34e559f99aa6f595fe25c0c42ed3084465f058
Ivan Galakhov <igalakhov00@stuy.edu>
2019-04-11 19:15:01 -0400

    working on stuff

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,16 @@ from flask import Flask
 from app.views.public.views import public_views
 from flask_migrate import Migrate
 from flask_session import Session
+import pymysql
 
 app = Flask(__name__)
 
 app.secret_key = SECRET_KEY
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = LOCAL_DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 
+pymysql.install_as_MySQLdb()  # lol
 db.init_app(app)
 
 with app.app_context():
