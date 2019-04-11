0473495a663ba54d48827ed1d04c29fe7d9f56b8
Ivan Galakhov <igalakhov00@stuy.edu>
2019-04-10 22:42:03 -0400

    sessions work

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,23 @@ from app.models import db
 from flask import Flask
 from app.routes import thing
 from flask_migrate import Migrate
+from flask_session import Session
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = SECRET_KEY
+app.secret_key = SECRET_KEY
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 
-# db.init_app(app)
-#
-# with app.app_context():
-#     db.drop_all()
-#     db.create_all()
+db.init_app(app)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
 migrate = Migrate(app, db)
+session = Session(app)
+
 
 app.register_blueprint(thing)
